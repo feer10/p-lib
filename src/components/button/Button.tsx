12d3d79bf16ee7cb5beb184cff1c3ec8cf68b6dd
Fer Hidalgo <fer@Air-de-Fer.bbrouter>
2023-05-19 18:51:11 +0200

    feat(button): add data testid

--- src/components/button/Button.tsx
+++ src/components/button/Button.tsx
@@ -4,12 +4,13 @@ import './Button.scss'
 export interface ButtonProps {
   children: string | JSX.Element | JSX.Element[] | ReactNode
   className?: string
+  testId?: string
   onClick?: () => void
 }
 
-export const Button: FC<ButtonProps> = ({ children, onClick, className }) => {
+export const Button: FC<ButtonProps> = ({ children, onClick, className, testId }) => {
   return (
-    <button className={`custom-button ${className}`} onClick={onClick}>
+    <button className={`custom-button ${className}`} onClick={onClick} data-testid={testId}>
       {children}
     </button>
   )
